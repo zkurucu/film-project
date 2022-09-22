94d8d6593fb4c6630abbe2b8cd2c52b7e772d18f
Zafer Kurucu <z.kurucu.d@hotmail.com>
2022-09-22 13:18:26 +0300

    Add movies to UI

---
+++ project.js
@@ -0,0 +1,32 @@
+const form = document.getElementById("film-form");
+const titleElement = document.querySelector("#title");
+const directorElement = document.querySelector("#director");
+const urlElement = document.querySelector("#url");
+
+// UI objesini başlatma
+const ui = new UI();
+
+// Tüm eventleri yükleme
+eventListeners();
+
+function eventListeners() {
+  form.addEventListener("submit", addFilm);
+}
+function addFilm(e) {
+  const title = titleElement.value;
+  const director = directorElement.value;
+  const url = urlElement.value;
+
+  if (title === "" || director === "" || url === "") {
+    // Hata
+  } else {
+    // Yeni Film
+    const newFilm = new Film(title, director, url);
+    // Yeni filmi arayüze ekleme
+    ui.addFilmToUI(newFilm);
+  }
+
+  ui.clearInputs(titleElement, urlElement, directorElement);
+
+  e.preventDefault();
+}
